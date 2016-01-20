04cb0e6cbe4f9628c57ba0ccd8a5902ea10485a1
Robbie Lane <lane.robbie@gmail.com>
2016-01-20 10:07:30 -0700

    vid7

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -1,6 +1,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var ReactRouter = require('react-router')
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Navigation = ReactRouter.Navigation;
+var createBrowserHistory = require('history/lib/createBrowserHistory');
+
 /*
   App
 */
@@ -85,5 +91,27 @@ var StorePicker = React.createClass({
 
 });
 
+/*
+  Not Found
+*/
 
-ReactDOM.render(<App/>, document.querySelector('#main'));
+var NotFound = React.createClass({
+  render : function() {
+    return <h1>Not Found!</h1>
+  }
+});
+
+
+/*
+  Routes
+*/
+
+var routes = (
+    <Router history={createBrowserHistory()}>
+      <Route path="/" component={StorePicker} />
+      <Route path="/store/:storeId" component={App} />
+      <Route path="*" component={NotFound} />
+    </Router>
+  )
+
+ReactDOM.render(routes, document.querySelector('#main'));

2bced4ad2f3d450042968929dceaff002b49c8d3
Robbie Lane <lane.robbie@gmail.com>
2016-01-31 09:23:37 -0700

    vid20

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -184,6 +184,9 @@ var Header = React.createClass({
         <h3 className="tagline"><span>{this.props.tagline}</span></h3>
       </header>
       )
+  },
+  propTypes : {
+    tagline: React.PropTypes.string.isRequired
   }
 })
 
@@ -248,6 +251,12 @@ var Order = React.createClass({
         </CSSTransitionGroup>
       </div>
       )
+  },
+
+  propTypes : {
+    fishes : React.PropTypes.object.isRequired,
+    order : React.PropTypes.object.isRequired,
+    removeFromOrder : React.PropTypes.func.isRequired
   }
 })
 
@@ -282,6 +291,14 @@ var Inventory = React.createClass({
         <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </div>
     )
+  },
+
+  propTypes : {
+    addFish : React.PropTypes.func.isRequired,
+    loadSamples : React.PropTypes.func.isRequired,
+    fishes : React.PropTypes.object.isRequired,
+    linkState : React.PropTypes.func.isRequired,
+    removeFish : React.PropTypes.func.isRequired
   }
 })
 

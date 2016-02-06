af7d949ce753e5379b00ae3314942f1e78122070
Robbie Lane <lane.robbie@gmail.com>
2016-02-06 09:31:20 -0700

    vid24

--- 23 - ES6 Classes/scripts/components/StorePicker.js
+++ 23 - ES6 Classes/scripts/components/StorePicker.js
@@ -1,4 +1,4 @@
-/* 
+/*
   StorePicker
   This will let us make <StorePicker/>
 */
@@ -6,16 +6,19 @@
 import React from 'react';
 import { History } from 'react-router';
 import h from '../helpers';
+import reactMixin from 'react-mixin';
 
-var StorePicker = React.createClass({
-  mixins : [History],
-  goToStore : function(event) {
+class StorePicker extends React.Component {
+  mixins : [History]
+
+  goToStore(event) {
     event.preventDefault();
     // get the data from the input
     var storeId = this.refs.storeId.value;
     this.history.pushState(null, '/store/' + storeId);
-  },
-  render : function() {
+  }
+
+  render() {
     return (
       <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
@@ -25,6 +28,8 @@ var StorePicker = React.createClass({
     )
   }
 
-});
+}
+
+reactMixin.onClass(StorePicker, History)
 
 export default StorePicker;

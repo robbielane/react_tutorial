c500e0d56994195f9309c368f070c3fbeab2b911
Robbie Lane <lane.robbie@gmail.com>
2016-02-06 09:25:55 -0700

    vid23

--- 23 - ES6 Classes/scripts/components/NotFound.js
+++ 23 - ES6 Classes/scripts/components/NotFound.js
@@ -4,11 +4,10 @@
 
 import React from 'react';
 
-var NotFound = React.createClass({
-  render : function() {
+class NotFound extends React.Component {
+  render() {
     return <h1>Not Found!</h1>
   }
-});
-
+}
 
 export default NotFound;
